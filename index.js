6588b2644b6372230932240d7c1da8315cba43ea
DestinHan <seung4530@naver.com>
2025-05-06 15:11:14 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// index.js
+const express = require('express')
+const app = express()
+const port = 1122
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`)
+})
