84d210d7bfad64e144f0bfe9527b24f00c274b75
DestinHan <seung4530@naver.com>
2025-05-07 12:34:06 -0400

    UPDATED

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-// index.js
 const express = require('express')
 const app = express()
 const port = 1122
+
+const bodyParser = require('body-parser')
+
+const { User } = require("./models/User")
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}))
+
+// application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://DestinHan:70714530hoon%40@cluster0.tyudykg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
@@ -13,6 +22,22 @@ mongoose.connect('mongodb+srv://DestinHan:70714530hoon%40@cluster0.tyudykg.mongo
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.post('/register', (req, res) => {
+  // information for new user signup from client
+  // put into database
+  
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false, err})
+    return res.status(200).json({
+      success: true})
+  })
+
+})
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
