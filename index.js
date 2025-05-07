2789f38fcdcb73ce56fcc0e58032fdb64b40eabf
DestinHan <seung4530@naver.com>
2025-05-07 11:01:24 -0400

    UPDATED

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const express = require('express')
 const app = express()
 const port = 1122
 
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://DestinHan:70714530hoon%40@cluster0.tyudykg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+    useNewUrlParser:true, useUnifiedTopology: true
+}).then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log('Error:', err))
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
